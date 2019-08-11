0dc40c0d74a82a90113d7a81e582e9ce02d9fb46
Quinyi Dong <chloe@Quinyis-MacBook-Pro.local>
2019-08-11 08:08:11 -0700

    Updates

---
+++ precache-manifest.6d639af6beb158e46bc178a7922fb706.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4134462fef47c62f0832dc8e7d45c49c",
+    "url": "/monsters_rolodex/index.html"
+  },
+  {
+    "revision": "6a48833c8b264458d27b",
+    "url": "/monsters_rolodex/static/css/main.2060d5fa.chunk.css"
+  },
+  {
+    "revision": "eb5d03ae7c9a30028659",
+    "url": "/monsters_rolodex/static/js/2.4e81ba5b.chunk.js"
+  },
+  {
+    "revision": "6a48833c8b264458d27b",
+    "url": "/monsters_rolodex/static/js/main.bd8cfea6.chunk.js"
+  },
+  {
+    "revision": "49b65f9e2948fbe95569",
+    "url": "/monsters_rolodex/static/js/runtime~main.31c0adc3.js"
+  }
+]);
